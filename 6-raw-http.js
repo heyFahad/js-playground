b36bf80d43832c6baf4e731c1d0280bf8c972c29
Fahad J <Fahad_Javed008@yahoo.com>
2020-01-28 18:49:05 +0500

    HTTP Requests Without a Library

---
+++ 6-raw-http.js
@@ -0,0 +1,22 @@
+const https = require("https");
+const url =
+  "https://api.darksky.net/forecast/ab909e938a8ef73e0a294061c91bc734/40,-75";
+
+const request = https.request(url, response => {
+  let data = "";
+
+  response.on("data", chunk => {
+    data = data + chunk.toString();
+  });
+
+  response.on("end", () => {
+    const body = JSON.parse(data);
+    console.log(body);
+  });
+});
+
+request.on("error", error => {
+  console.log("An error", error);
+});
+
+request.end();
